20314b11fd0bc4989d4e1694d8b6c1e1c4fe0a3f
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-25 12:38:31 +0000

    done

--- App.js
+++ App.js
@@ -4,22 +4,18 @@ const path = require('path');
 
 const app = express();
 
-// Define the path to your JSON file
-const filePath = path.join(__dirname, 'user.json'); // Assuming user.json is in the same directory as app.js
+const filePath = path.join(__dirname, 'user.json'); 
 
-// Define a GET endpoint to read the JSON file
+
 app.get('/user', (req, res) => {
-  // Read the JSON file
+
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    // Parse the JSON data
     try {
       const user = JSON.parse(data);
-      // Send the user data as a response
       res.json(user);
     } catch (parseError) {
       console.error('Error parsing JSON:', parseError);
@@ -28,7 +24,7 @@ app.get('/user', (req, res) => {
   });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
