64c9a7e36c6a0099f6f7fb0e0e3bf28889a92936
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-26 05:03:49 +0000

    sddsf

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -16,7 +17,6 @@ app.get("/fake",(req,res)=>{
      res.json({"res":"true"});
 })
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
