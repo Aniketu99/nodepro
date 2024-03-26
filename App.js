63c4a2931d0a51eb890169750b497f2fc636b84a
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-26 06:24:44 +0000

    done registration

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const express = require('express');
+const { json } = require('body-parser');
 const app = express();
 
 app.use(express.json());
@@ -17,9 +18,20 @@ app.get("/user", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  var userData = req.body;
-  res.json({ userData });
+
+  try {
+    const registerData = req.body;
+    const userData = fs.readFileSync("user.json", "utf8");
+    const user = JSON.parse(userData);
+    user.push(registerData);
+    fs.writeFileSync("user.json", JSON.stringify(user));
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
