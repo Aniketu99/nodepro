81da4dcfe65f6008b3cba5286af75ff798b5917a
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-26 05:27:22 +0000

    done

--- App.js
+++ App.js
@@ -1,34 +1,23 @@
 const fs = require('fs');
 const express = require('express');
-const { json } = require('body-parser');
 const app = express();
 
 app.use(express.json());
 
 app.get("/user", (req, res) => {
   try {
-  
     const userData = fs.readFileSync("user.json", "utf8");
-
     const user = JSON.parse(userData);
-
     res.json({ user });
-
   } catch (error) {
-
     console.error("Error reading user data:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.post("/register",(req,res)=>{
-
-     var userData = req.body;
-
-     var data = json.parse(userData);
-
-    res.json({data});
-     
+app.post("/register", (req, res) => {
+  var userData = req.body;
+  res.json({ userData });
 });
 
 const PORT = process.env.PORT || 3000;
