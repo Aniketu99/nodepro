5ecfe1b79485fe980267d3735557aa5516fe0725
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-26 12:01:14 +0000

    done

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ app.post("/login", (req, res) => {
 
     const logData = JSON.parse(login);
     
-    res.json(logData[0].email);
+    res.json(logData[1].email);
     
     // if (logData[i].email == email && logData[i].password == password) {
     //   currentuser = user;
