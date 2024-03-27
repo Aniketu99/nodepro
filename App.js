9d2bc3b32f9ba018141ea395afe34822284a0ade
Aniketu99 <161241425+Aniketu99@users.noreply.github.com>
2024-03-27 09:33:45 +0000

    done

--- App.js
+++ App.js
@@ -83,8 +83,11 @@ app.get("/currentuser",(req,res)=>{
      res.json({currentuser});
      
 });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+
   console.log(`Server is listening on port ${PORT}`);
+  
 });
